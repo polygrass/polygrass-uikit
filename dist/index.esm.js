a322b6e4200581756cd1d67da0d1bbc9e366481d
Flavio Soares <flavios.soares@hotmail.com>
2021-06-28 11:20:59 -0300

    change dark colors

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2871,7 +2871,7 @@ var brandColors = {
 var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#273b4f", textDisabled: "#BDC2C4", textSubtle: "#273b4f", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#51c4d3", background: "#002700", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#191326", input: "#eeeaf4", primaryDark: "#FFFFFF", tertiary: "#353547", text: "#c3ffc3", textDisabled: "#BDC2C4", textSubtle: "#c3ffc3", borderColor: "#524B63", card: "#FFFFFF", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#51c4d3", background: "#005000", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#191326", input: "#eeeaf4", primaryDark: "#002700", tertiary: "#353547", text: "#c3ffc3", textDisabled: "#BDC2C4", textSubtle: "#c3ffc3", borderColor: "#524B63", card: "#002700", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
